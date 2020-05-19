2be872b25cacc613dc9da8a019bb8f3c692b8133
Shanmugarajeshwaran R <shanmugarajeshwaranr@gmail.com>
2020-05-19 10:10:41 +0530

    Initial Commit

---
+++ ImageUploaderExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageUploaderExample
+//
+//  Created by ADMIN on 19/05/20.
+//  Copyright © 2020 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
