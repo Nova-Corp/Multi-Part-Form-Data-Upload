275c11398346111622e6724e354e82269ec1b24b
Shanmugarajeshwaran R <shanmugarajeshwaran@sparkouttech.com>
2022-03-04 20:48:30 +0530

    Cleanup

---
+++ ImageUploaderExample/Delegate/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  ImageUploaderExample
+//
+//  Created by ADMIN on 19/05/20.
+//  Copyright © 2020 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard (scene as? UIWindowScene) != nil else { return }
+    }
+}
+
